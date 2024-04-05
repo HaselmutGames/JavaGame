9377696b17ff77fd28ae2922f9a5e9308a8bef2a
Hammoudiii13 <hammoud.aldalati@rwth-aachen.de>
2024-04-05 22:26:08 +0200

    Game loop and thread created

--- src/com/mime/javagame/Display.java
+++ src/com/mime/javagame/Display.java
@@ -3,10 +3,39 @@ package com.mime.javagame;
 import javax.swing.*;
 import java.awt.*;
 
-public class Display extends Canvas {
+public class Display extends Canvas implements Runnable {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     public static final String TITLE = "Java Game";
+
+    private Thread thread;
+    private boolean running = false;
+
+    private void start() {
+        if (running) return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+
+        System.out.println("Working!");
+    }
+
+    private void stop() {
+        if (!running) return;
+        running = false;
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public void run() {
+        while(running) {
+
+        }
+    }
 
     public static void main(String[] args) {
         Display game = new Display();
@@ -19,5 +48,7 @@ public class Display extends Canvas {
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
+
+        game.start();
     }
 }
